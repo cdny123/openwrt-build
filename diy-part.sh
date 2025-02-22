b3cdd7bf678e440a6bc99a130fa2d35a10e74eca
cdny123 <133751542+cdny123@users.noreply.github.com>
2025-02-22 18:22:48 +0800

    Update diy-part.sh

--- diy-part.sh
+++ diy-part.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 # 添加自定义软件源
 echo "src-git custom1 https://github.com/cdny123/openwrt-package1.git" >> feeds.conf.default
 
@@ -16,6 +18,7 @@ git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-a
 sed -i 's/KERNEL_PATCHVER:=.*/KERNEL_PATCHVER:=6.6/g' target/linux/x86/Makefile
 
 # 添加个性签名, 默认增加年月日
+mkdir -p package/base-files/files/etc
 echo "DISTRIB_DESCRIPTION='OpenWrt $(date +%Y-%m-%d)'" >> package/base-files/files/etc/openwrt_release
 
 # 添加默认主题为argon
@@ -23,10 +26,10 @@ sed -i 's/luci.main.mediaurlbase=.*/luci.main.mediaurlbase=\/luci-static\/argon/
 
 # 下载 OpenClash 和 adguardhome 的内核文件
 mkdir -p files/etc/openclash/core
-curl -o files/etc/openclash/core/clash https://github.com/vernesong/OpenClash/releases/download/TUN-Premium/clash-linux-amd64.tar.gz
+curl -L -o files/etc/openclash/core/clash https://github.com/vernesong/OpenClash/releases/download/TUN-Premium/clash-linux-amd64.tar.gz
 
 mkdir -p files/usr/bin
-curl -o files/usr/bin/AdGuardHome https://github.com/AdguardTeam/AdGuardHome/releases/latest/download/AdGuardHome_linux_amd64.tar.gz
+curl -L -o files/usr/bin/AdGuardHome https://github.com/AdguardTeam/AdGuardHome/releases/latest/download/AdGuardHome_linux_amd64.tar.gz
 
 # 修改 openwrt 后台地址为 192.168.6.1，默认子网掩码：255.255.255.0，修改主机名称为OP-NIT
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
@@ -46,8 +49,14 @@ echo "vm.min_free_kbytes=65536" >> package/base-files/files/etc/sysctl.conf
 echo "kernel.sched_migration_cost_ns=5000000" >> package/base-files/files/etc/sysctl.conf
 echo "kernel.sched_autogroup_enabled=1" >> package/base-files/files/etc/sysctl.conf
 
+# 更新和安装feeds
+./scripts/feeds update -a
+./scripts/feeds install -a
+
 # 运行 OpenWrt 编译
 make defconfig
 make -j$(nproc)
-# 将生成的固件文件复制到 /build/bin
-cp bin/targets/x86/64/* /build/bin/
+
+# 将生成的固件文件复制到 /build/output
+mkdir -p /build/output
+cp bin/targets/x86/64/* /build/output/
